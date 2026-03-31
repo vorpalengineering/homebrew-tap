b4c3cf90fb2ca9fa8a9230023d4e80c7c99ca6d2
Craig <craig.branscom@gmail.com>
2026-03-31 01:36:22 -0400

    Vorpal CLI v0.1.1

--- Formula/vorpal.rb
+++ Formula/vorpal.rb
@@ -7,16 +7,16 @@ class Vorpal < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/vorpalengineering/cli/releases/download/v#{version}/vorpal-v#{version}-darwin-arm64.tar.gz"
-      sha256 "461da297c73aafaf2983056333c8cbb618e2957fcd9b6af2ef21eba6c73689ca"
+      sha256 "66b6b009064e67a8587dc7de52671a311be3834e6bfdeb9895041a9534a6ee4c"
     else
       url "https://github.com/vorpalengineering/cli/releases/download/v#{version}/vorpal-v#{version}-darwin-amd64.tar.gz"
-      sha256 "eda56439fed8e822932d7f9f34943bdfc3764cf34745a3be1c5f8a04f9ec46ed"
+      sha256 "d32602983fc58fdbb2f78e4d98aba1e156c646d27aab923e2f9f7d080adf8a7d"
     end
   end
 
   on_linux do
     url "https://github.com/vorpalengineering/cli/releases/download/v#{version}/vorpal-v#{version}-linux-amd64.tar.gz"
-    sha256 "5a27fb505bcb8be8461134ac1ac996a0fb02861043217e3ced2dea742c8da94f"
+    sha256 "9e4830149659c904103c42e5c2ad1a657077f3e077911c40e74715e1b5297db9"
   end
 
   def install
