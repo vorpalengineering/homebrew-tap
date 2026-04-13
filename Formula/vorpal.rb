f4b59b0bffeef1679793e2d57776de11cbedc96c
Craig <craig.branscom@gmail.com>
2026-04-13 19:30:54 -0400

    Vorpal CLI v0.1.3

--- Formula/vorpal.rb
+++ Formula/vorpal.rb
@@ -1,22 +1,22 @@
 class Vorpal < Formula
-  desc "CLI tool for the Vorpal Engineering security platform"
+  desc "CLI tool for the Vorpal Engineering Blockchain Intelligence Platform"
   homepage "https://github.com/vorpalengineering/cli"
-  version "0.1.2"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/vorpalengineering/cli/releases/download/v#{version}/vorpal-v#{version}-darwin-arm64.tar.gz"
-      sha256 "01765876cc35c705296aeed37d23f7c7378e220b98df44bd604397992fa1fb6b"
+      sha256 "8032abebf8497700277fc0d51beef93924253a1a377dff3156de79735bd225e5"
     else
       url "https://github.com/vorpalengineering/cli/releases/download/v#{version}/vorpal-v#{version}-darwin-amd64.tar.gz"
-      sha256 "08a40b657b17f873a75011ebf53e5259fa3e5fea8ca6ddeca618d7103aa1e922"
+      sha256 "cfcda5e9666aa61bfa149c18e547c61f205af3ece2747582268fa138cb88e1c6"
     end
   end
 
   on_linux do
     url "https://github.com/vorpalengineering/cli/releases/download/v#{version}/vorpal-v#{version}-linux-amd64.tar.gz"
-    sha256 "bb99e4079a292e5ab312f28c6bc2e9ab11d56511a7acd372cd8878ec50625bd0"
+    sha256 "48791a523607e91565e27189e0d6ff19ab862cb88aea1a2f63f3a7c1df7307a7"
   end
 
   def install
